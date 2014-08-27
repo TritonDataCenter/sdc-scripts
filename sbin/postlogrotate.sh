a51d9eb5e4723ea1d484be0352905da66839a958
Josh Wilsdon <jwilsdon@joyent.com>
2014-08-27 11:36:12 -0700

    TOOLS-607 I for one, welcome my new open source license

--- sbin/postlogrotate.sh
+++ sbin/postlogrotate.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/bash
 # vi: sw=4 ts=4 et
 #
-# Copyright (c) 2013 Joyent Inc., All rights reserved.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+#
+# Copyright (c) 2014, Joyent, Inc.
+#
+
 #
 # The standard script to be used as the command for '-a CMD' in
 # /etc/logadm.conf for rotated SDC logs.  The primary function here is to
